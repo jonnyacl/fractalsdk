db211e94ef1d760f8f67187f269a46489296566c
Jonny Lea <jonny@fractal-labs.com>
2019-05-02 13:36:01 +0100

    config in setup command

--- api/accounting/index.js
+++ api/accounting/index.js
@@ -2,28 +2,28 @@ const axios = require('axios');
 const config = require("../../config").config
 const apiHelper = require("../helper").helper
 
-const bankAxios = axios.create({
+const acctAxios = axios.create({
     baseURL: config.acctUrl,
     timeout: 1000,
 })
 
 const getAccounts = (auth, bankId) => {
-    return bankAxios.get(`/banking/${bankId}/accounts`, { headers: apiHelper.createHeaders(auth) });
+    return acctAxios.get(`/banking/${bankId}/accounts`, { headers: apiHelper.createHeaders(auth) });
 }
 
 const getAccountsById = (auth, bankId, accountId) => {
-    return bankAxios.get(`/banking/${bankId}/accounts/${accountId}`, { headers: apiHelper.createHeaders(auth) });
+    return acctAxios.get(`/banking/${bankId}/accounts/${accountId}`, { headers: apiHelper.createHeaders(auth) });
 }
 
 const getAccountTransactions = (auth, bankId, accountId) => {
-    return bankAxios.get(`/banking/${bankId}/accounts/${accountId}/transactions`, { headers: apiHelper.createHeaders(auth) });
+    return acctAxios.get(`/banking/${bankId}/accounts/${accountId}/transactions`, { headers: apiHelper.createHeaders(auth) });
 }
 
 const getAccountBalances = (auth, bankId, accountId) => {
-    return bankAxios.get(`/banking/${bankId}/accounts/${accountId}/balances`, { headers: apiHelper.createHeaders(auth) });
+    return acctAxios.get(`/banking/${bankId}/accounts/${accountId}/balances`, { headers: apiHelper.createHeaders(auth) });
 }
 
-exports.banking = {
+exports.accounting = {
     getAccounts,
     getAccountsById,
     getAccountBalances,
---
+++ api/company/index.js
@@ -0,0 +1,27 @@
+const axios = require('axios');
+const config = require("../../config").config
+const apiHelper = require("../helper").helper
+
+const compAxios = axios.create({
+    baseURL: config.companyUrl,
+    timeout: 1000,
+})
+
+const getCompanies = (auth) => {
+    return compAxios.get("/companies", { headers: apiHelper.createHeaders(auth) });
+}
+
+const getCompanieById = (auth, cId) => {
+    return compAxios.get(`/companies/${cId}`, { headers: apiHelper.createHeaders(auth) });
+}
+
+const createCompany = (auth, company) => {
+    return compAxios.post("/companies", company, { headers: apiHelper.createHeaders(auth) });
+}
+
+exports.company = {
+    getAccounts,
+    getAccountsById,
+    getAccountBalances,
+    getAccountTransactions
+}
