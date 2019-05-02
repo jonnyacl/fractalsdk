9c7cf7bc5d2b9a502d3f612ff773a64673db6d89
Jonny Lea <jonny@fractal-labs.com>
2019-05-02 15:14:40 +0100

    Add auth

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const bankApi = require("./api/banking").banking
+const authApi =require("./api/auth").auth
 
 const createBankApi = (apiKey, partner) => {
     return bankApi.createBankApi(apiKey, partner)
 }
 
+const createAuthApi = (apiKey, partner) => {
+    return authApi.createAuthApi(apiKey, partner)
+}
+
 exports.api = {
-    createBankApi
+    createBankApi,
+    createAuthApi
 }
