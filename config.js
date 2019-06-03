27c6ffe7df630f501a3d7102765d08ae230200d0
Viktor Kis <vkkis1993@gmail.com>
2019-06-03 13:43:50 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const version = "v1";
 
-const bankUrl = `https://xb88dje32d.execute-api.eu-west-1.amazonaws.com/${version}`;
-const acctUrl = `https://tv28sjxpq2.execute-api.eu-west-1.amazonaws.com/${version}`;
-const companyUrl = `https://uqkio28nsj.execute-api.eu-west-1.amazonaws.com/${version}`;
-const authUrl = `https://9opk7bt3ci.execute-api.eu-west-1.amazonaws.com/${version}`;
+const bankUrl = `https://r7p2rhg4ji.execute-api.eu-west-1.amazonaws.com/${version}`;
+const acctUrl = `https://13onkb74v6.execute-api.eu-west-1.amazonaws.com/${version}`;
+const companyUrl = `https://9i54hif0ii.execute-api.eu-west-1.amazonaws.com/${version}`;
+const authUrl = `https://7gq3bccyoa.execute-api.eu-west-1.amazonaws.com/${version}`;
 
 exports.config = {
     version,
